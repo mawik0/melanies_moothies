086ada16efcfd4307ca462694409c475a4a78dc6
mawik0 <84012620+mawik0@users.noreply.github.com>
2025-07-19 20:55:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
       smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/banana")
       st_sf = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
-        ingredients_string += fruit_chosen+ ' '
+      ingredients_string += fruit_chosen+ ' '
     st.write(ingredients_string)
      
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
